ba67dc0a60c339698a7f9bda205b7f0c59a915e9
韦荣桃 <59642814+peach-stack@users.noreply.github.com>
2021-06-17 09:20:20 +0800

    Add files via upload

---
+++ final-recognition/face_decet.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+cascade = cv.CascadeClassifier("model/haarcascade_frontalface_default.xml")  ## 读入分类器数据
+
+sample_image = cv.imread("images/more.jpg")  ## 图片地址
+
+faces = cascade.detectMultiScale(sample_image, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))
+
+for (x, y, w, h) in faces:
+    cv.rectangle(sample_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv.imwrite("./image_detection/more.jpg", sample_image)  ## 框出的人脸图片输出到a.png
+
+if len(faces) > 0:
+    print("存在" + str(len(faces)) + "张人脸")
+else:
+    print("不存在人脸")
